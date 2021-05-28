246a8222499c848efd2d03185076949ecbb46992
poltavski <onsub0@gmail.com>
2021-05-29 02:11:59 +0300

    Add u2net implementation. Update server to MVP

--- api/main.py
+++ api/main.py
@@ -2,14 +2,16 @@ from fastapi import FastAPI, File, HTTPException
 from PIL import Image
 from io import BytesIO
 
-# import numpy as np
+import numpy as np
 import logging
 import requests
 import sys
 import uvicorn
 from fastapi.staticfiles import StaticFiles
 
-# from fastapi.responses import FileResponse
+
+from models.u2nethttp import u2net
+from fastapi.responses import FileResponse
 # from utils import (
 # process_image,
 # process_plate_image,
@@ -21,7 +23,7 @@ from fastapi.staticfiles import StaticFiles
 # ColorRecognition,
 # DirectionRecognition,
 # )
-# from cv2 import cvtColor, imwrite, COLOR_RGB2BGR
+from cv2 import cvtColor, imwrite, COLOR_RGB2BGR
 from fastapi.middleware.cors import CORSMiddleware
 
 sys.setrecursionlimit(1500)
@@ -60,7 +62,13 @@ def analyse(image: Image):
     Returns:
         dict of recognized properties of vehicle items.
     """
-    pass
+    # Ensure i,qge size is under 1024
+    if image.size[0] > 1024 or image.size[1] > 1024:
+        image.thumbnail((1024, 1024))
+
+    # Process Image
+    results = u2net.run(np.array(image))
+    return results
 
 
 @app.get("/")
@@ -115,10 +123,9 @@ async def inference_demo(
     image = Image.open(BytesIO(response.content)).convert("RGB")
     result = analyse(image)
     if result:
-        pass
         # result_image = visualize_results(image, result)
-        # imwrite("result.jpg", cvtColor(result_image, COLOR_RGB2BGR))
-        # return FileResponse("result.jpg")
+        result.save("result.jpg", "JPEG")
+        return FileResponse("result.jpg")
 
 
 @app.get("/inference/url")
@@ -163,4 +170,4 @@ async def inference_file(file: bytes = File(...)):
 
 
 if __name__ == "__main__":
-    uvicorn.run("server:app", host="0.0.0.0", port=8050, log_level="info")
+    uvicorn.run("main:app", host="0.0.0.0", port=8050, log_level="info")
